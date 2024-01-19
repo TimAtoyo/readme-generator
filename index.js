740c1cc537f3d38c21f8fb5341a9590515330ad4
Stim Fry Mac <timothy.atoyo@gmail.com>
2024-01-19 18:55:40 +0000

    Adds user response variables in the readme template literal sting

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ const readmeString = `
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+${motivation}
+${build}
+${problem}
+${learning}
 
 ## Table of Contents (Optional)
 
@@ -27,11 +25,13 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+${instalSteps}
+
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
+${instructions}
+
 
 To add a screenshot, create an "assets/images" folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
 
@@ -49,7 +49,8 @@ If you followed tutorials, include links to those here as well.
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+${licenses}
+
 
 ---
 
@@ -62,6 +63,8 @@ The last section of a high-quality README file is the license. This lets other d
 Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
 
 ## Languages 
+
+${languages}
 ![image]({BadgeURLHere})
 https://img.shields.io/badge/JSS-F7DF1E?style=for-the-badge&logo=JSS&logoColor=white
 
@@ -74,6 +77,8 @@ If your project has a lot of features, list them here.
 If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
 
 ## Tests
+${testing}
+
 `
 
 
@@ -132,13 +137,19 @@ const questions = [
         default: "JavaScript",
         choices: ["HTML", "JavaScript", "C", "Python", "Other"],
       },
+      {
+        type: "input",
+        name: "test input",
+        message: "Instructions for testing",
+        default: "npm test",
+      },
 ];
 
 
 inquirer
   .prompt(questions)
   .then((answers) => {
-    // Use user feedback for... whatever!!
+    // Variables of vales from user feedback!
     const motivation = answers["motivation input"]
     const build = answers["build input"];
     const problem = answers["problem input"];
@@ -147,10 +158,11 @@ inquirer
     const instructions = answers["instructions input"];
     const licenses = answers["licenses input"];
     const languages = answers["languages input"];
+    const testing = answers["test input"];
 
 
 
-    writeFile("index.html", result, (err) =>
+    writeFile("readme.md", result, (err) =>
       // TODO: Describe how this ternary operator works
       err ? console.error(err) : console.log("Commit logged!")
     );
