40d2396aa0a1d75736cbc763b3fbdbf09370df1a
Stim Fry Mac <timothy.atoyo@gmail.com>
2024-01-21 16:04:32 +0000

    Refactors codebase using functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -31,8 +31,7 @@ function generateMarkdown(data) {
       
   To add a screenshot, create an "assets/images" folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
       
-          md
-          ![alt text](assets/images/screenshot.png)
+          md![alt text](assets/images/screenshot.png)
           
       
   ## Credits
